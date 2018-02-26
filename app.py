ac57e2ce5d73d8f81ecd0c4479483c3f6cbdd60e
avashishta5 <avashishta5@gmail.com>
2018-02-26 19:09:40 +0530

    Updated Templates

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 app = Flask(__name__)
 
 @app.route('/')
-def index:
-	return 'HOME'	
+def index():
+	return render_template('home.html')	
 
 if __name__ == '__main__':
 	app.secret_key = ''
